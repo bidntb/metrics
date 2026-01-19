82a7d1b881878da8b261d45e07433d0d584ff687
elizaveta.anisimova <elizaveta.anisimova@devim.team>
2026-01-20 00:32:00 +0300

    [add] separated router logic from app'

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,51 +1,22 @@
 package app
 
 import (
-	"bidntb/metrics/internal/middleware"
+	"bidntb/metrics/internal/nconfig"
+	"bidntb/metrics/internal/router"
 	"bidntb/metrics/internal/service/handler"
-	"bidntb/metrics/internal/service/logger"
 	"bidntb/metrics/internal/service/metrics"
 	"bidntb/metrics/internal/storage"
-
-	"github.com/gin-gonic/gin"
-
-	"bidntb/metrics/internal/nconfig"
 )
 
-func setupRouter(storage storage.Interface) *gin.Engine {
-	metricsSvc := metrics.NewService(storage)
-	h := handler.NewHandler(metricsSvc)
-
-	r := gin.New()
-	r.Use(logger.LoggingMiddleware())
-	r.Use(middleware.ErrorHandler(h.NotFoundHandler, h.BadRequestHandler))
-
-	r.GET("/", h.ListMetrics)
-	r.POST("/value", h.GetMetricJSON)
-	r.POST("/value/", h.GetMetricJSON)
-	r.POST("/update", h.UpdateMetric)
-	r.POST("/update/", h.UpdateMetric)
-	r.POST("/update/gauge/:name/:value", middleware.ValidateGaugeValue(), h.UpdateGauge)
-	r.POST("/update/counter/:name/:value", middleware.ValidateCounterValue(), h.UpdateCounter)
-	r.GET("/value/:type/:name", h.GetValue)
-
-	r.POST("/update/counter", h.NotFoundHandler)
-	r.POST("/update/gauge/", h.NotFoundHandler)
-	r.POST("/update/gauge", h.NotFoundHandler)
-	r.POST("/update/:wrong", h.BadRequestHandler)
-	r.POST("/update/:wrong/*any", h.BadRequestHandler)
-
-	r.NoRoute(h.NotFoundHandler)
-
-	return r
-}
 func Run() {
 	serverAddress := nconfig.GetServerAddress()
 	storageInstance := storage.NewMemStorage()
+	metricsSvc := metrics.NewService(storageInstance)
+	h := handler.NewHandler(metricsSvc)
 
-	router := setupRouter(storageInstance)
+	r := router.SetupRouter(h)
 
-	if err := router.Run(serverAddress); err != nil {
+	if err := r.Run(serverAddress); err != nil {
 		panic(err)
 	}
 }
---
+++ internal/router/router.go
@@ -0,0 +1,35 @@
+package router
+
+import (
+	"bidntb/metrics/internal/middleware"
+	"bidntb/metrics/internal/service/handler"
+	"bidntb/metrics/internal/service/logger"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(h *handler.Handler) *gin.Engine {
+	r := gin.New()
+
+	r.Use(logger.LoggingMiddleware())
+	r.Use(middleware.ErrorHandler(h.NotFoundHandler, h.BadRequestHandler))
+
+	r.GET("/", h.ListMetrics)
+	r.POST("/value", h.GetMetricJSON)
+	r.POST("/value/", h.GetMetricJSON)
+	r.POST("/update", h.UpdateMetric)
+	r.POST("/update/", h.UpdateMetric)
+	r.POST("/update/gauge/:name/:value", middleware.ValidateGaugeValue(), h.UpdateGauge)
+	r.POST("/update/counter/:name/:value", middleware.ValidateCounterValue(), h.UpdateCounter)
+	r.GET("/value/:type/:name", h.GetValue)
+
+	r.POST("/update/counter", h.NotFoundHandler)
+	r.POST("/update/gauge/", h.NotFoundHandler)
+	r.POST("/update/gauge", h.NotFoundHandler)
+	r.POST("/update/:wrong", h.BadRequestHandler)
+	r.POST("/update/:wrong/*any", h.BadRequestHandler)
+
+	r.NoRoute(h.NotFoundHandler)
+
+	return r
+}
